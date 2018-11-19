7552783831f3ca43189ce857167a48865a247d5c
xiaojiyansuan <43595389+xiaojiyansuan@users.noreply.github.com>
2018-11-19 23:43:45 +0800

    Update 2.c

--- 20181113/2.c
+++ 20181113/2.c
@@ -1,16 +1,38 @@
-#include<stdio.h>
-void main()
-void swap(int arr[], int i, int j) {
-	int temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
-}
-void BubbleSort(int arr[], int left, int right) {
-	int i, j;
-	for (i = 1; i <= right - left; ++i) {
-		for (j = left; j <= right - i; ++j) {
-			if (arr[j] > arr[j + 1])
-				swap(arr, j, j + 1);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void BubbleSort(int arr[], int left, int right);
+
+int main(){
+	srand(time(NULL));
+	int arr[100],left,right;
+	for(int i=0;i<100;i++){
+		arr[i]=rand()%10000;
+	    
+	}
+	scanf("%d %d",&left,&right);
+	for(int i=0;i<100;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	BubbleSort(arr,left,right);
+	for(int i=0;i<100;i++){
+		printf("%d ",arr[i]);
+	}
+	return 0;
+} 
+
+void BubbleSort(int arr[],int left,int right){
+	for(int i=left;i<right;i++){
+		int j,t;
+		for(j=i+1;j<=right;j++){
+			if(arr[i]>arr[j]){
+				t=arr[i];
+				arr[i]=arr[j];
+				arr[j]=t;
+			}
 		}
 	}
 }
+
