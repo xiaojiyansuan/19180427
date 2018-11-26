05d0a3d62a253f9e7930f84b1a7c80c62193fa98
xiaojiyansuan <43595389+xiaojiyansuan@users.noreply.github.com>
2018-11-26 20:23:45 +0800

    Create 2.c

---
+++ 20181120/2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int bin_insert(int n, int m, int j, int i) 
+{
+	m <<= j;
+	n |= m;
+	return n;
+}
+int main()
+{
+int n,m,j,i;
+scanf("%d %d",&n,&m);
+int bin_insert(n,m,j,i);
+}
